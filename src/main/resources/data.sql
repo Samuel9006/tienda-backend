fe2ee774a9ec4b3d8e3410729a68ae9927cc956b
samuel <samuel.villanueva.90@hotmail.com>
2024-07-12 20:25:25 -0500

    update sql data example

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 insert into producto (stock, valor, descripcion, nombre, imagen_path)  values
-(50, 8000, 'Membresia 1 mes de Netflix', 'Netflix-1m', 'productos/netflix.jpeg'),
-(20, 7000, 'Membresia 1 mes de amazon prime video', 'Amazon-1m', 'productos/amazon.jpeg'),
-(10, 15000, 'Membresia 1 mes de HBOmax', 'HBO-1m', 'productos/amazon.jpeg'),
-(60, 5500, 'Membresia 1 mes de spotify', 'Spotify-1m', 'productos/spotify.jpeg'),
-(30, 9000, 'Membresia 1 mes de canva', 'Canva-1m', 'productos/canva.jpeg'),
-(15, 10000, 'Membresia 1 mes de Claro Video', 'ClaroVideo-1m', 'productos/clarovideo.jpeg'),
-(40, 12000, 'Membresia 1 mes de Disney', 'Disney-1m', 'productos/disney.jpeg'),
-(9, 7000, 'Membresia 1 mes de Plex', 'Plex-1m', 'productos/plex.jpeg'),
-(33, 9000, 'Membresia 1 mes de Vix', 'Vix-1m', 'productos/vix.jpeg'),
-(65, 10000, 'Membresia 1 mes de Youtube', 'Youtube-1m', 'productos/youtube.jpeg');
+(50, 8000, 'Membresia 1 mes de Netflix', 'Netflix-1m', 'netflix.jpeg'),
+(20, 7000, 'Membresia 1 mes de amazon prime video', 'Amazon-1m', 'amazon.jpeg'),
+(10, 15000, 'Membresia 1 mes de HBOmax', 'HBO-1m', 'amazon.jpeg'),
+(60, 5500, 'Membresia 1 mes de spotify', 'Spotify-1m', 'spotify.jpeg'),
+(30, 9000, 'Membresia 1 mes de canva', 'Canva-1m', 'canva.jpeg'),
+(15, 10000, 'Membresia 1 mes de Claro Video', 'ClaroVideo-1m', 'clarovideo.jpeg'),
+(40, 12000, 'Membresia 1 mes de Disney', 'Disney-1m', 'disney.jpeg'),
+(9, 7000, 'Membresia 1 mes de Plex', 'Plex-1m', 'plex.jpeg'),
+(33, 9000, 'Membresia 1 mes de Vix', 'Vix-1m', 'vix.jpeg'),
+(65, 10000, 'Membresia 1 mes de Youtube', 'Youtube-1m', 'youtube.jpeg');
